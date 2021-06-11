f01f6279781912bff7df639178b914bb6449e4e4
Shardukar <85742031+Shardukar@users.noreply.github.com>
2021-06-11 13:27:35 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showString("3")
+        basic.showString("2")
+        basic.showString("1")
+        basic.showString("go")
+    }
+})
+basic.forever(function () {
+	
+})
