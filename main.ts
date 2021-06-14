7b22884142ee3fc7a8ada2755ea73d7319996d9d
Shardukar <85742031+Shardukar@users.noreply.github.com>
2021-06-14 12:32:04 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,11 +5,13 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("Go")
 })
 input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index < 4; index++) {
-        bitbot.setLedColor(0xFF0000)
-        bitbot.goms(BBDirection.Forward, 60, 400)
-        bitbot.rotatems(BBRobotDirection.Left, 100, 280)
-    }
+    bitbot.goms(BBDirection.Forward, 60, 400)
+    bitbot.rotatems(BBRobotDirection.Left, 60, 400)
+    bitbot.goms(BBDirection.Forward, 60, 400)
+    bitbot.rotatems(BBRobotDirection.Left, 60, 400)
+    bitbot.goms(BBDirection.Forward, 60, 400)
+    bitbot.rotatems(BBRobotDirection.Left, 60, 400)
+    bitbot.goms(BBDirection.Forward, 60, 400)
     bitbot.ledRainbow()
 })
 basic.showString("Press A to start")
